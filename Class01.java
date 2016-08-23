d770c6b0b6aa571fdae061a33ff6ea7d6d20a220
mukhtiarmemon <mukhtiar001@yahoo.com>
2016-08-23 16:26:43 +0500

    Create Class01.java

---
+++ Class01.java
@@ -0,0 +1,10 @@
+package com.test.git02;
+
+public class Class01Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("class 01");
+	}
+
+}
