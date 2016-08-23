037805d34184a567ecde35a940d8508673c69fdc
mukhtiarmemon <mukhtiar001@yahoo.com>
2016-08-23 14:28:56 +0500

    test 012 commit

--- src/com/test/git02/ClassTesting.java
+++ src/com/test/git02/ClassTesting.java
@@ -5,6 +5,7 @@ public class ClassTesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test 011");
+		System.out.println("test 012");
 
 
 	}
